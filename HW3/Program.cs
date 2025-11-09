94fc05bac3689ed980824ea2d3c30f091284d499
ArturKrush <arturkrush@gmail.com>
2025-11-09 22:27:15 +0200

    ISort interface was implemented in MyArray class, SortByParam() method was tested in main()

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -8,7 +8,7 @@ namespace Homework3
         {
             //Task 1
             int[] array = [ 1, 10, 12, 20, 35 ];
-            /*string info = "After casting to the interface type...";
+            string info = "After casting to the interface type...";
 
             MyArray myArray = new(array);
 
@@ -25,6 +25,26 @@ namespace Homework3
             Console.WriteLine(mathInterface.Min()); //Результат: 1
             Console.WriteLine(mathInterface.Avg()); //Результат: 15
             Console.WriteLine(mathInterface.Search(12)); //True
+
+            //Task 3
+            int[] unSortedArr = [ 90, 100, 10, 15, 22, 111 ];
+            MyArray myArray3 = new(unSortedArr);
+
+            ISort sortInterface = myArray3;
+            bool isAsc = true;
+            sortInterface.SortByParam(isAsc);
+
+            Console.WriteLine("The view of the array sorted by ascending:");
+            myArray3.Show();
+
+            int[] unSortedArr2 = [86, 192, 76, 12, 110, 10];
+            MyArray myArray4 = new(unSortedArr2);
+            ISort sortInterface2 = myArray4;
+            isAsc = false;
+            sortInterface2.SortByParam(isAsc);
+
+            Console.WriteLine("The view of the array sorted by descending:");
+            myArray4.Show();
         }
     }
 }
