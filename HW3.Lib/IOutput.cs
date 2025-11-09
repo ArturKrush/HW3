3eb32cb6d91ee6c857183738b0264b8e3665b299
ArturKrush <arturkrush@gmail.com>
2025-11-09 17:20:29 +0200

    IOutput interface was implemented in MyArray class, main() was designed for changes

--- HW3.Lib/IOutput.cs
+++ HW3.Lib/IOutput.cs
@@ -5,6 +5,13 @@ namespace Homework3
 {
     public interface IOutput
     {
+        public void Show(); /*Цей метод обов'язково має бути реалізованим
+        у MyArray, якщо MyArray реалізовує цей інтерфейс*/
 
+        public void Show(string info)
+        {
+            Console.WriteLine(info + "It is an IOutput interface realization:");
+            Show();
+        }
     }
 }
--- HW3/Program.cs
+++ HW3/Program.cs
@@ -6,7 +6,16 @@ namespace Homework3
     {
         public static void Main(string[] args)
         {
+            int[] array = [ 1, 10, 12, 20, 35 ];
+            string str = "After casting to the interface type...";
 
+            MyArray myArray = new(array);
+            //myArray.Show();
+
+            IOutput outputInterface = myArray;
+            outputInterface.Show(); //фактично виклик методу Show() класу MyArray 
+            outputInterface.Show(str); /* фактично виклик методу Show(string info)
+            класу MyArray, відбувається перекриття реалізації за замовчуванням з інтерфесу */
         }
     }
 }
