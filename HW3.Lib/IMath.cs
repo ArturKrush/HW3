97b4f7f5561c65461e87ffb7137228beaa5394b2
ArturKrush <arturkrush@gmail.com>
2025-11-09 21:12:07 +0200

    IMath interface was implemented in MyArray class, methods were tested in main()

---
+++ HW3.Lib/IMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    public interface IMath
+    {
+        public int Max();
+        public int Min();
+        public float Avg();
+        bool Search(int valueToSearch);
+    }
+}
--- HW3.Lib/MyArray.cs
+++ HW3.Lib/MyArray.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Homework3
 {
-    public class MyArray : IOutput
+    public class MyArray : IOutput, IMath
     {
         public int[] MyArr
         {
@@ -31,6 +31,51 @@ namespace Homework3
         {
             Console.WriteLine(info + " It is a MyArray class realization:");
             Show();
+        }
+
+        public int Max()
+        {
+            int max = MyArr[0];
+            for (int i = 1; i < MyArr.Length; i++)
+            {
+                if (MyArr[i] > max)
+                    max = MyArr[i];
+            }
+            return max;
+        }
+
+        public int Min()
+        {
+            int min = MyArr[0];
+            for (int i = 1; i < MyArr.Length; i++)
+            {
+                if (MyArr[i] < min)
+                    min = MyArr[i];
+            }
+            return min;
+        }
+
+        public float Avg()
+        {
+            int sum = 0;
+            for (int i = 0; i < MyArr.Length; i++)
+            {
+                sum += MyArr[i];
+            }
+            return sum/MyArr.Length;
+        }
+
+        /*Оскільки пошук може відбуватися як в відсортованому, так і в невідсортованому масиві
+        то знайти елемент швидше за O(n) за допомогою бінарного пошуку не вдасться,
+        так як попередньо треба відсортувати масив*/
+        public bool Search(int valueToSearch)
+        {
+            foreach (int item in MyArr)
+            {
+                if(item == valueToSearch)
+                    return true;
+            }
+            return false;
         }
     }
 }
